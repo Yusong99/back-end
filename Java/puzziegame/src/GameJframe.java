65e53d813412496dbcbf996fb5900cf1439f49a9
Yusong99 <yusongxu1999@gmail.com>
2024-01-26 23:35:18 +0800

    2024.1.26 23:35

--- Java/puzziegame/src/GameJframe.java
+++ Java/puzziegame/src/GameJframe.java
@@ -7,6 +7,42 @@ public class GameJframe extends JFrame {
     // 以后游戏相关的所有逻辑都写在这个类
 
     public GameJframe() throws HeadlessException {
+        // ctrl + alt + m
+        initFrame();
+        initMenu();
+        // 让界面显示出来，建议写在后面
+        this.setVisible(true);
+    }
+
+    private void initMenu() {
+        // init menu
+        JMenuBar jMenuBar = new JMenuBar();
+        // 创建菜单的选项
+        JMenu functionJMenu = new JMenu("function");
+        JMenu aboutJMenu = new JMenu("about us");
+
+        // 创建选项下面的条目对象
+        JMenuItem replayItem = new JMenuItem("重新游戏");
+        JMenuItem reLoginItem = new JMenuItem("重新登陆");
+        JMenuItem closeItem = new JMenuItem("关闭游戏");
+
+        JMenuItem accountItem = new JMenuItem("公众号");
+
+        // 将每一个选项下面的条目添加到选项中
+        functionJMenu.add(replayItem);
+        functionJMenu.add(reLoginItem);
+        functionJMenu.add(closeItem);
+        aboutJMenu.add(accountItem);
+
+        // 将菜单里面的两个选项添加到菜单中
+        jMenuBar.add(functionJMenu);
+        jMenuBar.add(aboutJMenu);
+
+        // 给整个界面设置菜单
+        this.setJMenuBar(jMenuBar);
+    }
+
+    private void initFrame() {
         // 设置界面的宽高
         this.setSize(400, 400);
         // 标题
@@ -19,12 +55,5 @@ public class GameJframe extends JFrame {
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         // 可见性
         this.setVisible(true);
-
-        // init menu
-        JMenuBar jMenuBar = new JMenuBar();
-        // 创建菜单的选项
-        JMenu functionJMenu = new JMenu("function");
-        JMenu aboutJMenu = new JMenu("about us");
-
     }
 }
