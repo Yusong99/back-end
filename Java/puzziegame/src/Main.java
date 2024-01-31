1fab2065f1fc438b6404dce75742b73d0d64a727
Yusong99 <yusongxu1999@gmail.com>
2024-01-31 23:40:27 +0800

    2024.1.31 23:40

---
+++ Java/puzziegame/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 表示程序的启动入口
+        System.out.println("Hello world!");
+//        new LoginJframe();
+//        new RegisterJframe();
+        new GameJframe();
+    }
+}
