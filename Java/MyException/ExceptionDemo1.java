d97c0892781fecbb354fe563777a53811ad728f6
Yusong99 <yusongxu1999@gmail.com>
2024-02-12 00:23:45 +0800

    2024.2.12 00:23

--- Java/MyException/ExceptionDemo1.java
+++ Java/MyException/ExceptionDemo1.java
@@ -10,8 +10,11 @@ public class ExceptionDemo1 {
             // 拿着这个对象到catch的小括号中对比，看括号中的变量是否可以接收这个对象
             // 当catch里面所有的代码执行完毕，继续执行try...catch体系下面的其他代码
         }catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("索引越界了");
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            System.out.println(e.toString());
         }
+        System.err.println("123123");
         // 如果要捕获的异常存在父子关系，父类要写在下面
         System.out.println("仍在执行");
         //JDK 7之后可以用|来表示多种异常同一种处理方式
