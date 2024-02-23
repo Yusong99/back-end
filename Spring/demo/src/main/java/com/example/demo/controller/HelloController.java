b1349d0e108e406f049fc0737375f3479db09e51
Yusong99 <yusongxu1999@gmail.com>
2024-02-23 23:59:01 +0800

    2024.2.23 23:58

--- Spring/demo/src/main/java/com/example/demo/controller/HelloController.java
+++ Spring/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,18 +1,31 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * HelloController
  */
 // 请求处理类
+// @RestController
+// public class HelloController {
+// @RequestMapping("/hello")
+// public String requestMethodName(HttpServletRequest request) {
+// String name = request.getParameter("name");
+// String age = request.getParameter("age");
+// return name + age;
+// }
+// }
+// springboot方法
 @RestController
 public class HelloController {
     @RequestMapping("/hello")
-    public String requestMethodName() {
-        return "hello";
+    // 对于简单参数来说get和post都一样
+    public String requestMethodName(@RequestParam(name = "name1", required = false)String name, Integer age) {
+        return name + ":" + age;
     }
 }
