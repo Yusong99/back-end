e5c35c53ba3e6fa706f840274a192d6bb794ef6a
Yusong99 <yusongxu1999@gmail.com>
2024-02-24 10:01:14 +0800

    2024.2.24 10:01

--- Spring/demo/src/main/java/com/example/demo/controller/HelloController.java
+++ Spring/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -2,10 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.servlet.http.HttpServletRequest;
+import com.example.demo.pojo.User;
+
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 /**
  * HelloController
@@ -25,7 +28,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloController {
     @RequestMapping("/hello")
     // 对于简单参数来说get和post都一样
-    public String requestMethodName(@RequestParam(name = "name1", required = false)String name, Integer age) {
+    public String requestMethodName(@RequestParam(name = "name1", required = false) String name, Integer age) {
         return name + ":" + age;
+    }
+
+    @RequestMapping("/pojo")
+    public String requestMethodName(User user) {
+        System.out.println(user);
+        return "OK";
+    }
+
+    @RequestMapping("/complexPojo")
+    public String complexPojo(User user) {
+        System.out.println(user);
+        return "OK";
     }
 }
